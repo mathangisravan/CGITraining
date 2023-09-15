fb9c714bc79e93496d159a8637c09af22e3484b8
Mathangi Sravan <100198330+mathangisravan@users.noreply.github.com>
2023-09-15 14:13:17 +0530

    Delete emp1.sql

--- emp1.sql
+++
@@ -1,6 +0,0 @@
-
-
-INSERT INTO employees(first_name ,last_name ,email ,birthdate)
-VALUES ('Mathangi','Sravan','mathangisravan7@gmail.com','2000-12-02');
-INSERT INTO employees(first_name,last_name,email,birthdate)
-VALUES ('Macherla','Rajesh','macherlarajesh33@gmail.com','2001-01-01');
